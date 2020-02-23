e3127ac38329064eca5508618cf2cade82d3cdfa
Boopya <rikiyaeduarteyamamoto@gmail.com>
2020-02-23 15:15:40 +0800

    Change database host

--- AccessCredentials.java
+++ AccessCredentials.java
@@ -1,5 +1,5 @@
 public interface AccessCredentials {
-    public final static String DATABASE_HOST = "jdbc:derby://localhost:1527/libsystem";
+    public final static String DATABASE_HOST = "jdbc:oracle:thin:@localhost:1521:libsysdb";
     public final static String DATABASE_USERNAME = "okichinko";
     public final static String DATABASE_PASSWORD = "okichinko";
 }
