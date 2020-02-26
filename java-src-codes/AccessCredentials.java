92443d8f9f4021e6c567815359d65011908f9d8b
Boopya <rikiyaeduarteyamamoto@gmail.com>
2020-02-26 17:33:34 +0800

    delete all files

--- java-src-codes/AccessCredentials.java
+++
@@ -1,5 +0,0 @@
-public interface AccessCredentials {
-    public final static String DATABASE_HOST = "jdbc:oracle:thin:@localhost:1521:libsysdb";
-    public final static String DATABASE_USERNAME = "okichinko";
-    public final static String DATABASE_PASSWORD = "okichinko";
-}
