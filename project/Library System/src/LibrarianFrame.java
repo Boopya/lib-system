f3a9fd380caef10d101c987904d716eaed4cd10d
Rajan Elio <r-elio@users.noreply.github.com>
2020-02-29 00:51:21 +0800

    initialize basic objects for outline

--- project/Library System/src/LibrarianFrame.java
+++ project/Library System/src/LibrarianFrame.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.sql.*;
@@ -6,10 +7,47 @@ public class LibrarianFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private Connection con;
 
+	private JTabbedPane tabbedPane;
+	private JPanel transactionPanel;
+	private JPanel patronPanel;
+	private JPanel bookPanel;
+	private JPanel librarianPanel;
+
+	private JPanel[] panels = {transactionPanel,patronPanel,bookPanel,librarianPanel};
+	private String[] panelTitles = {"Transaction","Patron","Book","Librarian"};
+
+	private JLabel searchLabel;
+	private JTextField searchField;
+	private JComboBox<String> searchBox;
+
+	private JTable table;
+
+	private JButton addButton;
+	private JButton editButton;
+	private JButton deleteButton;
+	private JButton finishButton;
+
 	public LibrarianFrame(Connection con){
 		this.con = con;
 
 		setTitle("Librarian");
+
+		tabbedPane = new JTabbedPane();
+
+		for (int i = 0; i < panels.length; ++i){
+			panels[i] = new JPanel(new GridBagLayout());
+			tabbedPane.add(panelTitles[i],panels[i]);
+		}
+		tabbedPane.setFocusable(false);
+
+		addButton = new JButton("Add");
+		editButton = new JButton("Edit");
+		deleteButton = new JButton("Delete");
+		finishButton = new JButton("Finish");
+
+		table = new JTable();
+
+		add(tabbedPane);
 
 		pack();
 		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
