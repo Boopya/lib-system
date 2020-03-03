f4b88495d004cc3a1f1b35fc80db40ca304e3116
Boopya <rikiyaeduarteyamamoto@gmail.com>
2020-03-03 18:09:20 +0800

    refactor code

--- project/Library System/src/LoginFrame.java
+++ project/Library System/src/LoginFrame.java
@@ -4,13 +4,13 @@ import java.beans.*;
 import javax.swing.*;
 import java.sql.*;
 
-public class LoginFrame extends JFrame {
+public class LoginFrame extends JFrame implements SQLStatements{
 	private static final long serialVersionUID = 1L;
 	private Connection con;
 	private MenuBar menuBar;
 	private Menu aboutMenu;
-	private MenuItem aboutSys;
-	private MenuItem aboutUs;
+	private MenuItem aboutSystemMenuItem;
+	private MenuItem aboutUsMenuItem;
 
 	private JLabel loginLabel;
 	private JLabel passwordLabel;
@@ -20,90 +20,98 @@ public class LoginFrame extends JFrame {
 
 	public LoginFrame(Connection con){
 		this.con = con;
-
+		initComponents();
+	}
+	
+	public void initComponents() {
+		// frame settings
 		setTitle("Library System");
+		setLayout(new GridBagLayout());
+		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 
+		// menu objects
 		menuBar = new MenuBar();
-
 		aboutMenu = new Menu("About");
-
-		aboutSys = new MenuItem("About System");
-		aboutUs = new MenuItem("About Us");
-
-		aboutSys.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent event){
-				JOptionPane.showMessageDialog(rootPane,"[Message]","About System",JOptionPane.INFORMATION_MESSAGE);
-			}
-		});
-		aboutUs.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent event){
-				JOptionPane.showMessageDialog(rootPane,"[Message]","About Us",JOptionPane.INFORMATION_MESSAGE);
-			}
-		});
-
-		aboutMenu.add(aboutSys);
-		aboutMenu.add(aboutUs);
-
-		menuBar.add(aboutMenu);
-
-		setMenuBar(menuBar);
-
-		loginLabel = new JLabel("Login ID:");
-		passwordLabel = new JLabel("Password:");
-
-		loginField = new JTextField();
-		passwordField = new JPasswordField();
+		aboutSystemMenuItem = new MenuItem("About System");
+		aboutUsMenuItem = new MenuItem("About Us");
 		
+		// compile menu objects
+		aboutMenu.add(aboutSystemMenuItem);
+		aboutMenu.add(aboutUsMenuItem);
+		menuBar.add(aboutMenu);
+		setMenuBar(menuBar);
+		
+		// layout constraints
+		GridBagConstraints c = new GridBagConstraints();
+		c.insets = new Insets(5,5,5,5);
+		
+		// login label
+		loginLabel = new JLabel("Login ID:");
+		c.gridx = 0;
+		c.gridy = 0;
+		c.anchor = GridBagConstraints.EAST;
+		add(loginLabel,c);
+						
+		// password label
+		passwordLabel = new JLabel("Password:");
+		c.gridx = 0;
+		c.gridy = 1;
+		c.anchor = GridBagConstraints.EAST;
+		add(passwordLabel,c);
+		
+		// login field
+		loginField = new JTextField();
 		loginField.setPreferredSize(new Dimension(150,20));
-		passwordField.setPreferredSize(new Dimension(150,20));
-
 		loginField.addKeyListener(new LoginListener());
-		passwordField.addKeyListener(new LoginListener());
+		c.gridx = 1;
+		c.gridy = 0;
+		c.anchor = GridBagConstraints.WEST;
+		add(loginField,c);
 
+		// password field
+		passwordField = new JPasswordField();
+		passwordField.setPreferredSize(new Dimension(150,20));
+		passwordField.addKeyListener(new LoginListener());
+		c.gridx = 1;
+		c.gridy = 1;
+		c.anchor = GridBagConstraints.WEST;
+		add(passwordField,c);
+
+		// login button
 		loginButton = new JButton("Login");
 		loginButton.setPreferredSize(new Dimension(65,25));
-		loginButton.setFocusable(false);
 		loginButton.addActionListener(new LoginListener());
-
-		GridBagConstraints constraints = new GridBagConstraints();
-		constraints.insets = new Insets(5,5,5,5);
-		setLayout(new GridBagLayout());
+		loginButton.setFocusable(false);
+		c.gridx = 0;
+		c.gridy = 2;
+		c.gridwidth = 2;
+		c.anchor = GridBagConstraints.CENTER;
+		add(loginButton,c);
 		
-		constraints.gridx = 0;
-		constraints.gridy = 0;
-		constraints.anchor = GridBagConstraints.EAST;
-		add(loginLabel,constraints);
-
-		constraints.gridx = 1;
-		constraints.gridy = 0;
-		constraints.anchor = GridBagConstraints.WEST;
-		add(loginField,constraints);
-
-		constraints.gridx = 0;
-		constraints.gridy = 1;
-		constraints.anchor = GridBagConstraints.EAST;
-		add(passwordLabel,constraints);
-
-		constraints.gridx = 1;
-		constraints.gridy = 1;
-		constraints.anchor = GridBagConstraints.WEST;
-		add(passwordField,constraints);
-
-		constraints.gridx = 0;
-		constraints.gridy = 2;
-		constraints.anchor = GridBagConstraints.CENTER;
-		constraints.gridwidth = 2;
-		add(loginButton,constraints);
-
 		pack();
-		setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		
+		// about system action listener
+		aboutSystemMenuItem.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent event){
+				JOptionPane.showMessageDialog(rootPane, "[Message]", "About System", JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
+		
+		// about us action listener
+		aboutUsMenuItem.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent event){
+				JOptionPane.showMessageDialog(rootPane, "[Message]", "About Us", JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
+
+		// window listener
 		addWindowListener(new WindowAdapter(){
 			@Override
 			public void windowClosing(WindowEvent event){
-				int response = JOptionPane.showConfirmDialog(rootPane, "Do you want to exit?", 
-				"Exit", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+				int response = JOptionPane.showConfirmDialog(rootPane, "Do you want to exit?", "Exit", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+				
 				if (response == JOptionPane.YES_OPTION){
 					try {
 						con.close();
@@ -120,44 +128,45 @@ public class LoginFrame extends JFrame {
 	}
 
 	private void login(){
-		String loginStr = loginField.getText();
-		String passStr = String.valueOf(passwordField.getPassword());
+		PreparedStatement preparedStatement;
+		ResultSet resultSet;
+		String loginId, password;
+		boolean noUsersFound, isLibrarian;
+		
+		loginId = loginField.getText();
+		password = String.valueOf(passwordField.getPassword());
 
-		if (loginStr.isEmpty() || passStr.isEmpty()){
-			JOptionPane.showMessageDialog(rootPane, 
-			"You must fill all the required fields.", 
-			"Null Input", JOptionPane.WARNING_MESSAGE);
+		if (loginId.isEmpty() || password.isEmpty()){
+			JOptionPane.showMessageDialog(rootPane, "You must fill all the required fields.", "Null Input", JOptionPane.WARNING_MESSAGE);
 			return;
 		}
 
 		try {
-			String query = "SELECT LOGINID FROM PATRON WHERE LOGINID = ? AND PASSWORD = ?";
-			PreparedStatement ps = con.prepareStatement(query);
-			ps.setString(1, loginStr);
-			ps.setString(2, passStr);
+			preparedStatement = con.prepareStatement(PATRON_QUERY);
+			preparedStatement.setString(1, loginId);
+			preparedStatement.setString(2, password);
+			resultSet = preparedStatement.executeQuery();
 
-			ResultSet rs = ps.executeQuery();
-
-			if (!rs.next()){
-				JOptionPane.showMessageDialog(rootPane, "Invalid login ID/password.", 
-				"Login Failed", JOptionPane.ERROR_MESSAGE);
+			noUsersFound = !resultSet.next();
+			
+			if(noUsersFound){
+				JOptionPane.showMessageDialog(rootPane, "Invalid login ID or password.", "Login Failed", JOptionPane.ERROR_MESSAGE);
 				return;
 			}
 			
-			query = "SELECT LOGINID FROM LIBRARIAN WHERE LOGINID = ?";
-			ps = con.prepareStatement(query);
-			ps.setString(1,loginStr);
-			rs = ps.executeQuery();
+			preparedStatement = con.prepareStatement(LIBRARIAN_QUERY);
+			preparedStatement.setString(1, loginId);
+			resultSet = preparedStatement.executeQuery();
 
-			if (rs.next()){
-				createOptionDialog();
-			}
-			else {
-				createPatronFrame();
-			}
-
-			rs.close();
-			ps.close();
+			isLibrarian = resultSet.next();
+			
+			if(isLibrarian)
+				showSelectModeInterface();
+			else
+				showPatronFrame();
+			
+			preparedStatement.close();
+			resultSet.close();
 		}
 		catch (SQLException e){
 			e.printStackTrace();
@@ -166,33 +175,46 @@ public class LoginFrame extends JFrame {
 		}
 	}
 
-	private void createOptionDialog(){
-		Object[] options = {"Librarian","Patron"};
-		JOptionPane optionPane = new JOptionPane("Select the mode interface:\n", JOptionPane.INFORMATION_MESSAGE,
-		JOptionPane.OK_CANCEL_OPTION,null,options);
-		JDialog dialog = new JDialog(this,"Librarian Detected",true);
-		dialog.setContentPane(optionPane);
-		optionPane.addPropertyChangeListener(new PropertyChangeListener(){
+	private void showSelectModeInterface(){
+		Object[] modeInterfaceOptions;
+		String librarian, patron;
+		final int AS_LIBRARIAN, AS_PATRON;
+		
+		librarian = "Librarian";
+		AS_LIBRARIAN = 0;
+		
+		patron = "Patron";
+		AS_PATRON = 1;
+		
+		modeInterfaceOptions = new Object[]{librarian, patron};
+		
+		JOptionPane selectModeInterfacePane = new JOptionPane("Select the mode interface:\n", JOptionPane.INFORMATION_MESSAGE, JOptionPane.OK_CANCEL_OPTION, null, modeInterfaceOptions);
+		
+		JDialog dialog = new JDialog(this, "Librarian Detected", true);
+		dialog.setContentPane(selectModeInterfacePane);
+		dialog.setLocationRelativeTo(rootPane);
+		dialog.setVisible(true);
+		
+		selectModeInterfacePane.addPropertyChangeListener(new PropertyChangeListener(){
 			@Override
 			public void propertyChange(PropertyChangeEvent event) {
 				if (JOptionPane.VALUE_PROPERTY.equals(event.getPropertyName())){
-					if (optionPane.getValue().equals(options[0])){
-						optionPane.setValue(JOptionPane.UNINITIALIZED_VALUE);
-						createLibrarianFrame();
+					if (selectModeInterfacePane.getValue().equals(modeInterfaceOptions[AS_LIBRARIAN])){
+						selectModeInterfacePane.setValue(JOptionPane.UNINITIALIZED_VALUE);
+						showLibrarianFrame();
 					}
-					else if (optionPane.getValue().equals(options[1])){
-						optionPane.setValue(JOptionPane.UNINITIALIZED_VALUE);
-						createPatronFrame();
+					else if (selectModeInterfacePane.getValue().equals(modeInterfaceOptions[AS_PATRON])){
+						selectModeInterfacePane.setValue(JOptionPane.UNINITIALIZED_VALUE);
+						showPatronFrame();
 					}
 				}
 			}
 		});
+		
 		dialog.pack();
-		dialog.setLocationRelativeTo(rootPane);
-		dialog.setVisible(true);
 	}
 
-	private void createPatronFrame(){
+	private void showPatronFrame(){
 		PatronFrame patron = new PatronFrame(con);
 		patron.setSize(900,600);
 		patron.setResizable(false);
@@ -201,7 +223,7 @@ public class LoginFrame extends JFrame {
 		dispose();
 	}
 
-	private void createLibrarianFrame(){
+	private void showLibrarianFrame(){
 		try {
 			LibrarianFrame librarian = new LibrarianFrame(con);
 			librarian.setSize(900,600);
@@ -211,8 +233,7 @@ public class LoginFrame extends JFrame {
 			dispose();
 		} catch (SQLException e) {
 			e.printStackTrace();
-			JOptionPane.showMessageDialog(rootPane, e.getMessage(), 
-			"SQLException", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(rootPane, e.getMessage(), "SQLException", JOptionPane.ERROR_MESSAGE);
 		}
 		
 	}
